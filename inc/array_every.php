6b50b8bb5d87ca5ffb28a8fe97519538275377e0
Matt Milburn <me@mattmilburn.com>
2020-09-28 12:54:44 -0500

    Add array_every function

---
+++ inc/array_every.php
@@ -0,0 +1,21 @@
+<?php
+
+if ( ! function_exists( 'array_every' ) ) {
+  /**
+   * Returns true if the $cb for every item in $arr returns true.
+   *
+   * @param string   $list Array.
+   * @param callable $cb   Callback returning true or false.
+   *
+   * @return boolean
+   */
+  function array_every( array $arr, callable $cb ): bool {
+    foreach ( $arr as $item ) {
+      if ( ! $cb( $item ) ) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
